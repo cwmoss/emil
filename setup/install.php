3414f40ab46fb7b98c2ca0331bad7fcb169b20a2
rw <rw@20sec.net>
2021-03-08 01:21:19 +0100

    no more projects, dok corrections

--- setup/install.php
+++ setup/install.php
@@ -47,17 +47,17 @@ $msg = <<<EMSG
 	next you should enter your SMTP details in .env file
 
 	you can start your local server now
-	php -S localhost:1199 -t public/
+	php -S localhost:1199 -t public/ public/index.php
 
 	you can start by creating an organization ("acme")
-	curl -v localhost:1199/admin/create/acme -X POST -H "X-Emil-Admin: {$msecret}"
+	curl -v localhost:1199/admin/org/acme -X POST -H "X-Emil-Admin: {$msecret}"
 
 	this will create a api key for acme plus some templates to get startet
 	you can now send a message ("welcome") to yourself like this:
 
 	curl -v localhost:1199/send/acme/welcome -X POST \
 		-H "X-Emil-Api: acme-api-key-insert-here" \
-		-d '{"to":"youremail@yourserver.net","from":"youremail@yourserver.net","name":"strange guy"}'
+		-d '{"to":"youremail@yourserver.net","from":"youremail@yourserver.net","name":"Emil Smith"}'
 
 	have fun!  
 
