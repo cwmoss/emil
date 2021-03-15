dd1ac60f86937ae8348bfee99d0608cc86f7a793
Robert Wagner <rw@20sec.net>
2021-03-15 14:19:52 +0100

    generate jwt secret in setup

--- setup/install.php
+++ setup/install.php
@@ -1,37 +1,38 @@
 <?php
-$base = __DIR__."/../";
+$base = __DIR__ . '/../';
 
-require_once($base."src/helper.php");
+require_once $base . 'src/helper.php';
 
-$envf=$base.".env";
+$envf = $base . '.env';
 $secret = gen_secret();
 $passwd = gen_password();
 $passwd_hash = password_hash($passwd, PASSWORD_DEFAULT);
+$jwt_secret = gen_jwt_secret();
 
 if (!file_exists($envf)) {
     $secret = gen_secret();
-    
+
     print "generating EMIL_ADMIN_SECRET:\n$secret\n";
     print "createing .env from dot.env.example\n";
 
     file_put_contents(
         $envf,
-        str_replace(['super_secret_admin_key'], [$secret], file_get_contents($base."dot.env.example"))
+        str_replace(['super_secret_admin_key'], [$secret], file_get_contents($base . 'dot.env.example'))
     );
 } else {
     print ".env found in root folder, skip\n";
 }
 
 $writeable = [
-    'etc' => $base."etc",
-    'logs' => $base."logs",
-    'templates' => $base."templates",
+    'etc' => $base . 'etc',
+    'logs' => $base . 'logs',
+    'templates' => $base . 'templates',
 ];
 
 foreach ($writeable as $name => $dir) {
     print "checking $name ($dir) ";
     if (!file_exists($dir)) {
-        print "... creating ";
+        print '... creating ';
         mkdir($dir);
     }
 
@@ -43,7 +44,7 @@ foreach ($writeable as $name => $dir) {
     print "... ok\n";
 }
 
-$msecret = $secret?:"your_admin_secret_insert_here";
+$msecret = $secret ?: 'your_admin_secret_insert_here';
 
 $msg = <<<EMSG
 	
@@ -52,10 +53,12 @@ $msg = <<<EMSG
 	next you should enter your SMTP details in .env file
 
 	here are some example credentials
-	EMIL_ADMIN_KEY={$secret}
-	EMIL_ADMIN_PWD={$passwd_hash}
-		=> ui password: {$passwd}
 
+    EMIL_ADMIN_KEY={$secret}
+    EMIL_JWT_SECRET={$jwt_secret}
+    EMIL_ADMIN_PWD={$passwd_hash}
+		=> ui password: {$passwd}
+ 
 	you can start your local server now
 	php -S localhost:1199 -t public/ public/index.php
 
