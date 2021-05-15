6c0f5146b615e28ae2c7ca18f46ee13d83dd0a55
rw <rw@20sec.net>
2021-05-15 23:22:47 +0200

    markdown hb helper

--- src/emil/template/hb_helper.php
+++ src/emil/template/hb_helper.php
@@ -6,5 +6,9 @@ return [
     },
     'pluralize0' => function ($total, $zero, $singular, $plural) {
         return (!$total) ? $zero : ($total == 1 ? $singular : $plural);
+    },
+    'markdown' => function ($md) {
+        $pd = new Parsedown();
+        return $pd->text($md);
     }
 ];
