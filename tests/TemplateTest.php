2c023946c70c4191bf09f2786d91d93e1a293e32
Robert Wagner <rw@20sec.net>
2023-03-29 18:17:57 +0200

    tests

---
+++ tests/TemplateTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+error_reporting(\E_ALL);
+
+use PHPUnit\Framework\TestCase;
+
+final class TemplateTest extends TestCase {
+
+
+    public function setUp(): void {
+    }
+
+    public function testBasic(): void {
+    }
+}
